0de25308ca58b8707a28442068aced6cc27d9bd6
Pablo Cerda <pablo@citla.com>
2015-03-02 09:26:01 -0600

    Created shell script to execute the chart build command.

---
+++ achart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:~/python:~/python/PIL
+cd /Users/pablocc/Sites/Morinus
+/Library/Frameworks/Python.framework/Versions/2.7/bin/python chart_natal_dump.py PabloCerda 1983-07-13 23:45 33S27 70W38 -4
