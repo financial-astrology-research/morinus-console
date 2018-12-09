ed5c8b2a1c5fd209b05e37f7d3b64856dc0e5ce2
Pablo Cerda <pablo@citla.com>
2018-12-09 15:02:34 +0100

    Rename methods from prepare_* to create_*.

--- chart.py
+++ chart.py
@@ -30,7 +30,7 @@ import sys
 
 
 class Time:
-    """Time of Birth"""
+    """Date and time of the event used for chart calculation."""
 
     # Calendar type.
     GREGORIAN = 0
@@ -66,21 +66,52 @@ class Time:
         self.ph = None
 
         # Represent date / time as datetime object.
-        timezone = self.prepare_timezone(zone_hour, zone_minute)
-        self.date = datetime.datetime(
+        timezone = self.create_timezone(zone_hour, zone_minute)
+        self.date = self.create_datetime(
+            year, month, day, hour, minute, second, timezone, daylightsaving)
+        self.jd = self.convert_to_julian(self.date)
+
+        if full:
+            self.calcPHs(place)
+
+
+    def create_datetime(
+            self, year: int, month: int, day: int, hour: int, minute: int,
+            second: int, timezone: datetime.timezone, daylightsaving: bool):
+        """
+        Create a datetime object.
+
+        :param year: Date year.
+        :type year: int
+        :param month: Date month.
+        :type month: int
+        :param day: Date day.
+        :type day: int
+        :param hour: Time hour.
+        :type hour: int
+        :param minute: Time minutes.
+        :type minute: int
+        :param second: Time seconds.
+        :type second: int
+        :param timezone: Timezone of the time, will transform to UTC.
+        :type timezone: datetime.timezone
+        :param daylightsaving: Flag to indicate time has daylight saving.
+        :type daylightsaving: bool
+        """
+
+        date = datetime.datetime(
             year, month, day, hour, minute, second, 0, timezone)
 
         # Apply daylight balance when applies.
         if daylightsaving:
             date = date + datetime.timedelta(hours=-1)
 
-        self.jd = self.convert_to_julian(self.date)
-        if full:
-            self.calcPHs(place)
+        return date
 
-    def prepare_timezone(self, zone_hour: int, zone_minute: int):
+
+    def create_timezone(self, zone_hour: int, zone_minute: int):
         """
-        Prepare datetime timezone.
+        Create datetime timezone object.
 
         :param zone_hour: Timezone hour.
         :type zone_hour: int
@@ -89,6 +120,7 @@ class Time:
         :return: Datetime timezone object.
         :rtype: datetime.timezone
         """
+
         timezone_hours = zone_hour + (zone_minute / 60)
         timezone_delta = datetime.timedelta(hours=timezone_hours)
         timezone = datetime.timezone(timezone_delta)
@@ -121,6 +153,7 @@ class Time:
             calflag)
 
         return julian_date
+
 
     def calcPHs(self, place):
         # Planetary day/hour calculation
